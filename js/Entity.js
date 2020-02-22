5805375c224c1d4193675fab27e03839df1815e7
Ben <hubertben365@gmail.com>
2020-02-22 01:48:55 -0500

    Updated Physics and Entity File

--- js/Entity.js
+++ js/Entity.js
@@ -1,3 +1,20 @@
 /*
     Base class for entities in the game
 */
+
+class Entity{
+
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+
+        this.velocity = new Vector();
+    }
+
+    updatePosition(velocity, deltaTime){
+        this.x += this.velocity.x + deltaTime;
+        this.y += this.velocity.y + deltaTime;
+        
+    }
+
+}
---
+++ js/Physics.js
@@ -0,0 +1,22 @@
+
+class Physics{
+
+    constructor(entity, velocity = null) {
+        this.entity = entity;
+        this.velocity = velocity;
+    }
+
+    constructor(entity){
+        this.entity = entity;
+    }
+
+    start() {
+        // Starting Code
+    }
+
+    update(deltaTime) {
+        this.entity.updatePosition(this.velocity, deltaTime);
+    }
+
+
+}
